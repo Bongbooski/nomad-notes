13525e987b6bd0a478f8775a9fd06a4838b70e10
booski <p9346420@gmail.com>
2020-02-18 23:31:52 +0900

    ~#1.6 Add Note Mutation// still dev tools' doc part doesn't work

--- src/clientState.js
+++ src/clientState.js
@@ -1,5 +1,15 @@
+import { NOTE_FRAGMENT } from "./fragments";
+import { GET_NOTES } from "./queries";
+
 export const defaults = {
-  notes: []
+  notes: [
+    {
+      __typename: "Note",
+      id: 1,
+      title: "First",
+      content: "Second"
+    }
+  ]
 };
 export const typeDefs = [
   `
@@ -24,6 +34,32 @@ export const typeDefs = [
 ];
 export const resolvers = {
   Query: {
-    notes: () => []
+    note: (_, variables, { cache }) => {
+      const id = cache.config.dataIdFromObject({
+        __typename: "Note",
+        id: variables.id
+      });
+      console.log(id);
+      const note = cache.readFragment({ fragment: NOTE_FRAGMENT, id });
+      return note;
+    }
+  },
+  Mutation: {
+    createNote: (_, variables, { cache }) => {
+      const { notes } = cache.readQuery({ query: GET_NOTES });
+      const { title, content } = variables;
+      const newNote = {
+        __typename: "Note",
+        title,
+        content,
+        id: notes.length + 1
+      };
+      cache.writeData({
+        data: {
+          notes: [newNote, ...notes]
+        }
+      });
+      return newNote;
+    }
   }
 };
